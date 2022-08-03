9e37b38f309ea3e3dac7836b32236957c3ca3d91
Shivam Shukla <shivam99aa@gmail.com>
2022-08-03 22:19:07 +0530

    adding content-type header

--- photogallery_app/main.go
+++ photogallery_app/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my site </h1>")
+}
 
 func main()  {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
