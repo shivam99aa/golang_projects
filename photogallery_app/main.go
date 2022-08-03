5abe617fcf7782decfa67002d04c28a36e560e88
Shivam Shukla <shivam99aa@gmail.com>
2022-08-03 22:34:58 +0530

    adding 404 status page

--- photogallery_app/main.go
+++ photogallery_app/main.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to my site </h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch please send an email to <a href=\"mailto:shivam99aa@gmail.com\">shivam99aa@gmail.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>404, Page not Found</h1>")
 	}
 	
 }
