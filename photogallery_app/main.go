27e5d181f26ed9f7660bf45b6431a7c47a71f682
Shivam Shukla <shivam99aa@gmail.com>
2022-08-04 21:25:37 +0530

    adding faq and notfound handler page

--- photogallery_app/main.go
+++ photogallery_app/main.go
@@ -17,9 +17,25 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch please send an email to <a href=\"mailto:shivam99aa@gmail.com\">shivam99aa@gmail.com</a>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "FAQ Page")
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Custom 404 page</h1>")
+}
+
 func main()  {
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	http.ListenAndServe(":3000", r)
+	r.HandleFunc("/faq", faq)
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
