36bf06eabae7666088d2c8568d470275fbbea229
Shivam Shukla <shivam99aa@gmail.com>
2022-08-04 11:23:28 +0530

    moving from default mux to gorilla mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module golang_projects
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0
--- photogallery_app/main.go
+++ photogallery_app/main.go
@@ -3,22 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my site </h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch please send an email to <a href=\"mailto:shivam99aa@gmail.com\">shivam99aa@gmail.com</a>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>404, Page not Found</h1>")
-	}
-	
+	fmt.Fprint(w, "<h1>Welcome to my site </h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch please send an email to <a href=\"mailto:shivam99aa@gmail.com\">shivam99aa@gmail.com</a>")
 }
 
 func main()  {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
