02cf769933ebafd356e6395b7638bd5d39cd92ec
Shivam Shukla <shivam99aa@gmail.com>
2022-08-08 15:43:11 +0530

    created a simple contact view

--- photogallery_app/main.go
+++ photogallery_app/main.go
@@ -9,6 +9,7 @@ import (
 )
 
 var homeTemplate *template.Template
+var contactTemplate *template.Template
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -19,7 +20,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "To get in touch please send an email to <a href=\"mailto:shivam99aa@gmail.com\">shivam99aa@gmail.com</a>")
+	if err := contactTemplate.Execute(w, nil); err!= nil {
+		panic(err)
+	}
 }
 
 func faq(w http.ResponseWriter, r *http.Request) {
@@ -39,6 +42,11 @@ func main()  {
 	if err != nil {
 		panic(err)
 	}
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 	r.HandleFunc("/", home)
