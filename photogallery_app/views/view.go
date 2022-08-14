89fca4a4be6b04f1a4be5b3e9a377bc5dfa2070a
Shivam Shukla <shivam99aa@gmail.com>
2022-08-14 14:03:13 +0530

    added navbar template

--- photogallery_app/views/view.go
+++ photogallery_app/views/view.go
@@ -9,6 +9,7 @@ type View struct {
 
 func NewView(layout string, files ...string) *View {
 	files = append(files,
+		"views/layouts/navbar.gohtml",
 		"views/layouts/bootstrap.gohtml",
 		"views/layouts/footer.gohtml")
 
